e9142ad213dcaa67adc293489c9ad9811aca313d
Mike Schwörer <mailport@mikescher.de>
2015-12-24 14:30:34 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+
+namespace jClipCornLink
+{
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Thread.Sleep(3000);
+		}
+	}
+}
